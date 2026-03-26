1a8c49941d3bcf5c3cf781bf8c719acc87e2aabc
Chandini <chandini.nln@hashicorp.com>
2026-03-26 14:46:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+}
+
+provider "time" {}
+
+locals {
+  items = toset([for i in range(10) : tostring(i)])
+}
+
+resource "null_resource" "test" {
+  for_each = local.items
+
+  provisioner "local-exec" {
+    command = "echo Creating resource ${each.key}"
+  }
+}
+
+resource "time_sleep" "wait_after_each" {
+  for_each = null_resource.test
+
+  create_duration = "15s"
+}
